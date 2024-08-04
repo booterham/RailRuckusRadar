51f0dd3beb6c414b64ad78db8fd29d7a97c8c6b4
Bauke Blomme <bauke.blomme@student.hogent.be>
2024-08-04 18:51:15 +0200

    'wanneer duplicate info over zelfde vertrek, wordt enkel recentste bijgehouden door transform'

--- scripts/transforming.sh
+++ scripts/transforming.sh
@@ -56,10 +56,7 @@ fi
 touch "$TEMPFILE"
 
 for bestand in "$PARENT_DIR/$DIRNAME/"*; do
-    # check if file is newer than $TRANSFORMED_DATA_FILE, if $TRANSFORMED_DATA_FILE doesnt exist, every file will be newer
-    # if [ "$bestand" -nt "$TRANSFORMED" ]; then
-
-    # check if file has been fully loaded
+    # check if file has been loaded enough to be a usable format
     inhoud="$(cat "$bestand")"
     if [[ $inhoud == *"</liveboard>" ]]; then
         echo "$inhoud" >>"$TEMPFILE"
@@ -77,17 +74,18 @@ fi
 sed -i 's/<\/liveboard>/\n/g' "$TEMPFILE"
 sed -i 's/<liveboard[^<>]\+>//g' "$TEMPFILE"
 
-# formatting stations
+# departure stations
 ### station URI, id, locationX, locationY & standardname
 sed -i 's/^<station URI="[^"]\+" id="\([^"]\+\)" locationX="\([^"]\+\)" locationY="\([^"]\+\)" standardname="\([^"]\+\)">[^<>]\+<\/station>/\1;\2;\3;\4/g' "$TEMPFILE"
 
-# formatting destination stations
-sed -i 's/<departure id="[^"]\+" delay="\([^"]\+\)" canceled="\([^"]\+\)" left="\([^"]\+\)" isExtra="\([^"]\+\)"><station URI="\([^"]\+\)" id="\([^"]\+\)" locationX="\([^"]\+\)" locationY="\([^"]\+\)" standardname="\([^"]\+\)">[^<>]\+<\/station>/\n\1;\2;\3;\4;\5;\6;\7;\8;\9/g' "$TEMPFILE"
+# destination stations
+### delay, canceled, left, isExtra, destID, locationX, locationY, destname
+sed -i 's/<departure id="[^"]\+" delay="\([^"]\+\)" canceled="\([^"]\+\)" left="\([^"]\+\)" isExtra="\([^"]\+\)"><station URI="[^"]\+" id="\([^"]\+\)" locationX="\([^"]\+\)" locationY="\([^"]\+\)" standardname="\([^"]\+\)">[^<>]\+<\/station>/\n\1;\2;\3;\4;\5;\6;\7;\8/g' "$TEMPFILE"
 
 # formatting departure time, vehiclename, platform normal, platform and occupancy
 sed -i 's/<time formatted[^<>]\+>\([^<>]\+\)<[^<>]\+><vehicle[^<>]\+>\([^<>]\+\)<\/vehicle><platform normal="\([^"]\+\)">\([^<>]\+\)<\/platform><occupancy[^<>]\+>\([^<>]\+\)<\/occupancy>.*$/;\1;\2;\3;\4;\5/g' "$TEMPFILE"
 
-# removing the work "unknown" for unknown values and just put an empty string
+# removing the work "unknown" and "?" for unknown values and just put an empty string
 sed -i 's/unknown\|?//g' "$TEMPFILE"
 
 # formatting stations with zero departures
@@ -96,35 +94,52 @@ sed -i 's/<departures number="0"><\/departures>/\nnoDepartures/g' "$TEMPFILE"
 # remove departure numbers and end of departures section
 sed -i 's/<departures number="[0-9]\+">//g;s///g' "$TEMPFILE"
 
-# add a header to transformed data file
-if [ ! -s "$TRANSFORMED" ]; then
-    echo "depID;depLocX;depLocY;depName;delay;canceled;left;isExtra;liveURL;destID;destLocX;destLocY;destName;depTime;vehicleID;platformNormal;platform;occupancy" >>"$TRANSFORMED"
-fi
-
 # remove empty lines
 sed -i '/^\s*$/d' "$TEMPFILE"
 
 # merge departures with their station, then add these to the final file
 stationInfo=""
 while read -r line; do
-    echo "line$line line"
     if [[ "$line" == "BE"* ]]; then
         stationInfo="$line"
     else
         if [[ "$line" == "noDepartures" ]]; then
-            echo "$stationInfo;;;;;;;;;;;;;;" >>"$TRANSFORMED"
+            echo "$stationInfo;;;;;;;;;;;;;" >>"$TRANSFORMED"
         else
             echo "$stationInfo;$line" >>"$TRANSFORMED"
         fi
     fi
 done <"$TEMPFILE"
+
+# for liveboard info about the same train, only keep the last one since this one will have
+# most up-to-date information about delays etc.
+### put the delay column as
+
+# what info can change for specific train
+### delayed, canceled, left, platformnormal, platform, occupancy
+### col: 5, 6, 7, 15, 16, 17, so we need to leave these out when we sort
+
+# we run through the file and keep the last line, of the regex of the unique
+# identifiers of the last line (depstation, arrstation and vehID), matches
+# the current line, make the current line the last line. if it doesnt match, the last line has
+#the most up to date info and it can be added to transformed data file
+
+sort -t ';' -k1 -k9 -k13 "$TRANSFORMED" >"$TEMPFILE"
+
+# add a header to transformed data file
+echo "depID;depLocX;depLocY;depName;delay;canceled;left;isExtra;destID;destLocX;destLocY;destName;depTime;vehicleID;platformNormal;platform;occupancy" >"$TRANSFORMED"
+
+lastline=""
+while read -r line; do
+    thisline="$(echo "$line" | sed 's/\([^;]*;\)\([^;]*;\)\{7\}\([^;]*\);\([^;]*;\)\{3\}\([^;]*;\)[^;]*;[^;]*;[^;]*;[^;]*/\1\3\5/')"
+    if [[ "$lastline" != "$thisline" ]]; then
+        echo "$line" >>"$TRANSFORMED"
+    fi
+    lastline="$thisline"
+done <"$TEMPFILE"
+
 rm "$TEMPFILE"
 
-# # remove duplicate lines
-# mv "$TRANSFORMED" "$TEMPFILE"
-# uniq "$TEMPFILE" "$TRANSFORMED"
-
-# for liveboard info about the same train, only keep the last one since this one will have most up-to-date information about delays etc.
-# todo
+exit 0
 
 # rm "$TEMPFILE"
